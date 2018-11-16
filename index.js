c196d8e958089de57c293ad85b7070351d10c58c
Arnelle Balane <arnellebalane@gmail.com>
2018-11-16 23:41:09 +0800

    Add stylelint rules configurations

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: [
+        './rules/possible-errors.js',
+        './rules/language-features.js',
+        './rules/stylistic-issues.js'
+    ]
+};
---
+++ rules/language-features.js
@@ -0,0 +1,16 @@
+module.exports = {
+    rules: {
+        'color-named': 'never',
+        'function-url-no-scheme-relative': true,
+        'keyframes-name-pattern': '[a-z]+(-[a-z]+)*',
+        'shorthand-property-no-redundant-values': true,
+        'value-no-vendor-prefix': true,
+        'property-no-vendor-prefix': true,
+        'declaration-block-no-redundant-longhand-properties': true,
+        'declaration-no-important': true,
+        'declaration-block-single-line-max-declarations': 1,
+        'selector-max-empty-lines': 0,
+        'max-nesting-depth': 1,
+        'no-unknown-animations': true
+    }
+};
